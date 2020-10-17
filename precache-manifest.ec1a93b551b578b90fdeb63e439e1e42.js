2241e4152376cc968df8103508a15ffeb59f891c
DoVanQuyet <AnhQuyet9943.com>
2020-10-17 09:40:48 +0700

    Updates

---
+++ precache-manifest.ec1a93b551b578b90fdeb63e439e1e42.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d500adb82ceb109dbb6a3d7245d1bdb",
+    "url": "/react_landing_page/index.html"
+  },
+  {
+    "revision": "2b24c13a6601ffa46252",
+    "url": "/react_landing_page/static/css/2.ca816778.chunk.css"
+  },
+  {
+    "revision": "a144ce0b931b819e67ea",
+    "url": "/react_landing_page/static/css/main.1fb91da0.chunk.css"
+  },
+  {
+    "revision": "2b24c13a6601ffa46252",
+    "url": "/react_landing_page/static/js/2.3db65bd2.chunk.js"
+  },
+  {
+    "revision": "8a137f586fcebb1a370343eb47b7677c",
+    "url": "/react_landing_page/static/js/2.3db65bd2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a144ce0b931b819e67ea",
+    "url": "/react_landing_page/static/js/main.0ee24157.chunk.js"
+  },
+  {
+    "revision": "132d3a6d9d5c8362dfb6",
+    "url": "/react_landing_page/static/js/runtime-main.cbca9c16.js"
+  }
+]);
